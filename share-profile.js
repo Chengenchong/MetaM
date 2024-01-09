f3698077493b46b029eacfe95ba3d379c54c1774
LeeBritney <yixuanlee04@gmail.com>
2024-01-09 14:52:25 +0800

    ...

--- share-profile.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('copy-link-btn').addEventListener('click', function () {
-    // Add logic to copy the link to the clipboard
-    // You can use document.execCommand('copy') or the Clipboard API
-    // Example: navigator.clipboard.writeText('your_profile_url');
-    alert('Link copied to clipboard!');
-});
